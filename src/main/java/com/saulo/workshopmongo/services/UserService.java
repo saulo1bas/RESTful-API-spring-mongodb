a4337d0eb5821318ee5fcd1ffeaa2a1033277391
Saulo Bastos <saulo1bas@gmail.com>
2019-06-10 11:35:26 -0300

    Posting a User

--- src/main/java/com/saulo/workshopmongo/services/UserService.java
+++ src/main/java/com/saulo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.saulo.workshopmongo.domain.User;
+import com.saulo.workshopmongo.dto.UserDTO;
 import com.saulo.workshopmongo.repository.UserRepository;
 import com.saulo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
