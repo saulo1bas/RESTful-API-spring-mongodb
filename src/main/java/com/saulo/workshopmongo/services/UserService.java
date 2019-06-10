fc5e41c6b8b4ef1a7fc6812b2bff4d520b824535
Saulo Bastos <saulo1bas@gmail.com>
2019-06-10 10:55:16 -0300

    Using DTO

--- src/main/java/com/saulo/workshopmongo/services/UserService.java
+++ src/main/java/com/saulo/workshopmongo/services/UserService.java
@@ -1,20 +1,28 @@
 package com.saulo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.saulo.workshopmongo.domain.User;
 import com.saulo.workshopmongo.repository.UserRepository;
+import com.saulo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
